f162d17fb583bc6421bc46908ec17212df503240
rujanshakya <rujan.shakya123@gmail.com>
2023-02-22 17:33:15 +0545

    Added product model

---
+++ BikeStoreCollab/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace BikeStoreCollab.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDetails { get; set; }
+        public int Price { get; set; }
+    }
+}
