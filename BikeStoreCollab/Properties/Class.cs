e26a2632e401f29fe05bf36755e1d30f85901040
purushendra <purushendrakarn@gmail.com>
2023-02-24 08:52:52 +0545

    hi

---
+++ BikeStoreCollab/Properties/Class.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Security;
+
+namespace BikeStoreCollab.Properties
+{ 
+   class program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("hello world");
+            Console.ReadLine();
+        }
+    }
+}
+
